014569cf8b490163e7f05a874c93e7f7829dce20
Olga Gogoleva <mikadoolga@yandex.ru>
2024-08-08 10:11:49 +0300

    add public in test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -8,35 +8,35 @@ public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
     @Test
-    void shouldAdviseToBuy0() {
+    public void shouldAdviseToBuy0() {
 
         Assert.assertEquals(service.remain(1_000), 0);
 
     }
 
     @Test
-    void shouldAdviseToBuy100() {
+    public void shouldAdviseToBuy100() {
 
         Assert.assertEquals(service.remain(900), 100);
 
     }
 
     @Test
-    void shouldAdviseToBuy1() {
+    public void shouldAdviseToBuy1() {
 
         Assert.assertEquals(service.remain(999), 1);
 
     }
 
     @Test
-    void shouldAdviseToBuy999() {
+    public void shouldAdviseToBuy999() {
 
         Assert.assertEquals(service.remain(1), 999);
 
     }
 
     @Test
-    void shouldAdviseToBuy1000() {
+    public void shouldAdviseToBuy1000() {
 
         Assert.assertEquals(service.remain(0), 1000);
 
@@ -44,7 +44,7 @@ public class CashbackHackServiceTest {
 
 
     @Test
-    void shouldAdviseToBuyStill999() {
+    public void shouldAdviseToBuyStill999() {
 
         Assert.assertEquals(service.remain(1_001), 999);
 
